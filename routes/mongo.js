d94d82691741d74d1eb5cec086088c0ccc516b8b
J Prescott Sanders <jprescottsanders@gmail.com>
2013-03-21 19:35:43 -0400

    bootstrap

--- routes/mongo.js
+++ routes/mongo.js
@@ -7,10 +7,3 @@ exports.index = function(req, res){
 };
 
 
-exports.list = function(req, res){
-  res.render('list', { title: 'Mongo' });
-};
-
-exports.detail = function(req, res){
-  res.render('detail', { title: 'Mongo' });
-};
